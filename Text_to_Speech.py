3a736afce6e268c80334bedc7e1e3a0e5db91cf3
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-06-12 21:17:09 +0530

    Changes

--- Text_to_Speech.py
+++ Text_to_Speech.py
@@ -1,6 +1,6 @@
 from gtts import gTTS #Google Text to Speech API
 import os
-text = "My name is Ritesh Prasad Singh"
+text = "Hi, I am your assistance. How can I help you"
 language = 'en' #English
 speech = gTTS(text = text, lang = language, slow = False)
 speech.save("test.mp3")
