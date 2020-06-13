36b4290f869ef0bb91db3c5d0051f92786d2a67f
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-06-13 11:51:05 +0530

    :)

--- prediction.py
+++ prediction.py
@@ -4,13 +4,15 @@ ih_wgt = np.array([
                   [0.1, 0.2, -0.1],                   # hid[0]
                   [-0.1,0.1, 0.9],                    # hid[1]
                   [0.1, 0.4, 0.1]]).T                 # hid[2]
-                  
+print(ih_wgt)         
                                                       # hid[0] hid[1] hid[2]
 hp_wgt = np.array([
                    [0.3, 1.1, -0.3],                  # hurt?
                    [0.1, 0.2, 0.0],                   # win?
-                   [0.0, 1.3, 0.1] ]).T               # sad?
+                   [0.0, 1.3, 0.1]]).T  # sad?
+print(hp_wgt)
 weights = [ih_wgt, hp_wgt]
+print(weights)
 def neural_network(input, weights):
     hid = input.dot(weights[0])
     pred = hid.dot(weights[1])
@@ -18,7 +20,8 @@ def neural_network(input, weights):
 toes = np.array([8.5, 9.5, 9.9, 9.0])
 wlrec = np.array([0.65,0.8, 0.8, 0.9])
 nfans = np.array([1.2, 1.3, 0.5, 1.0])
-input = np.array([toes[0],wlrec[0],nfans[0]])
+input = np.array([toes[0], wlrec[0], nfans[0]])
+print(input)
 pred = neural_network(input,weights)
 print(pred)
 
