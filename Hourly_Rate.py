ba605b0d290722ac04a3a52265752a08c801d091
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-06-30 23:53:26 +0530

    update

---
+++ Hourly_Rate.py
@@ -0,0 +1,5 @@
+
+Hours = int(input("Enter Hours: "))
+Rate = float(input("Enter Rate: "))
+Pay = (float(round(Hours,2)) * float(round(Rate,2)))
+print("Pay: ",Pay )
--- Text_to_Speech.py
+++ Text_to_Speech.py
@@ -1,6 +1,6 @@
 from gtts import gTTS #Google Text to Speech API
 import os
-text = "Hi, I am your assistance. How can I help you"
+text = "Hello"
 language = 'en' #English
 speech = gTTS(text = text, lang = language, slow = False)
 speech.save("test.mp3")
