ff736d90515167fb816bf79b0697c4eb2f69a6b8
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-13 00:00:56 +0530

    Practising

--- Text_to_Speech.py
+++ Text_to_Speech.py
@@ -1,6 +1,6 @@
 from gtts import gTTS #Google Text to Speech API
 import os
-text = "Hello"
+text = "Hello  "
 language = 'en' #English
 speech = gTTS(text = text, lang = language, slow = False)
 speech.save("test.mp3")
