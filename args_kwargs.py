b00c756426be5868a003d8cae8ec594ecbbe73c6
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-14 23:00:31 +0530

    args

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,7 +1,18 @@
-def funargs(*args):
+def funargs(normal,*args):
     print(type(args))   #args are taken as tuple irrespective of the type
+    print(normal)
     print(args)
+    print(args[0])
+    print(args[2])
+    print(args[4])
+    print("......")
+    for item in args:
+        print(item)
     
+    
+
 Objects = ["Harry", "Rohan", "Skillf", "Hammad", "Shivam", "The Programmer"]
 
-funargs(*Objects)
+marks = 90
+
+funargs(marks,*Objects)
