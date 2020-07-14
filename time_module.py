d48c61a57378219ea862a4cac29dfa6f6f3bc0b9
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-15 00:07:54 +0530

    localtime

--- time_module.py
+++ time_module.py
@@ -1,15 +1,18 @@
 import time
 
-initial = time.time()
-print(initial)
-k = 0
-while(k<45):
-    print(f"Hi {k}")
-    k+=1
-print(time.time()-initial,"seconds")
+# initial = time.time()
+# print(initial)
+# k = 0
+# while(k<45):
+#     print(f"Hi {k}")
+#     k+=1
+# print(time.time()-initial,"seconds")
     
-initial2 = time.time()
-for i in range(45):
-    print("Hi")
-    k+=1
-print(time.time()-initial2,"seconds")
+# initial2 = time.time()
+# for i in range(45):
+#     print("Hi")
+#     k+=1
+# print(time.time()-initial2,"seconds")
+
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
