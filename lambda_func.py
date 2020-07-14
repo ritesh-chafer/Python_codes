39f4bc706ddb1b2a8b7f214bbb2c7fcce124bb0f
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-14 21:44:53 +0530

    lambda

---
+++ lambda_func.py
@@ -0,0 +1,12 @@
+ # Lambda Function or Anonymous Function
+ # def add (a,b):
+ #     return a + b
+
+# add = lambda x, y: x+y     #lambda function
+
+# print(add(2,3))
+
+a = [[1,4], [5,6], [8,23]]
+print(a[1])
+a.sort(key=lambda  x: x[1])
+print(a)
