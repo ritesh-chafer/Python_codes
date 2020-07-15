cd80058a1ab12439861930bedf896b0222506b26
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-16 03:01:39 +0530

    arithmetic operator

--- Arithmetic_operators.py
+++ Arithmetic_operators.py
@@ -4,6 +4,6 @@ print("Sum=",(a+b))                 # Addition
 print("Difference=",(a-b))          # Subtraction
 print("Product=",(a*b))             # Multiplication
 print("Quotient=",(a/b))            # Quotient
-print("Remainder=",(b%c))           # Remainder
+print("Remainder=",(b%c))           # Modulus/Remainder
 print("Exponent=",(b**2))           # Exponent
 print("Floor Division=",(b//c))     # Floor Division
