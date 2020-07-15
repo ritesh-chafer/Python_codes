8c8b943651d6c35a583192934b7c2d024307859b
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-16 03:40:26 +0530

    random functions

--- Random_numbers.py
+++ Random_numbers.py
@@ -7,3 +7,4 @@ print("shuffle(list):", random.shuffle(list))                     # shuffles the
 print("random.seed(20):", random.seed(20))                        # Gives the starting value for generatig a random number. Returns none.
 print("random():",random.random())                                # Returns a random floating-point number which lies between 0 and 1
 print("uniform(2,3):",random.uniform(2,3))                        # Generates random floating-point number n such that n>x and n<y
+print("randrange(2,10,1):",random.randrange(3,10,1))              # randrange(start,stop,step)
