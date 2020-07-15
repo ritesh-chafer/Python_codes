ab9f8e956afa9e591d4380b0e5df25e0f2c2577f
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-15 19:50:15 +0530

    changes

--- tutmain1.py
+++ tutmain1.py
@@ -2,7 +2,7 @@ def printhar(string):
     return f"This is a string {string}"
 
 def add(a,b):
-    return a + b + 5
+    return a + b + 4
 
 if __name__ == "__main__":
     print(printhar("Hello"))
