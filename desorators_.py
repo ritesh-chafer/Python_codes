54bf8f7f46947d7c5e04ff7603a0356adcc26fcb
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-15 22:07:08 +0530

    function in function

--- desorators_.py
+++ desorators_.py
@@ -1,6 +1,14 @@
-def function1():
-    print("Decorators")
+# def function1():
+#     print("Decorators")
     
-func2 = function1
-del function1
-func2()
+# func2 = function1
+# del function1
+# func2()
+
+def funcret(num):
+    if num == 0:
+        return print
+    if num == 1:
+        return int
+a = funcret(1)
+print(a)
