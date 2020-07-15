86db314aabf11b503212d3f7b974e42b03f6a70a
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-16 02:39:36 +0530

    :

--- Arithmetic_operators.py
+++ Arithmetic_operators.py
@@ -1,4 +1,6 @@
 a, b, c = 10, 5, 2
 
-print("Sum=",(a+b))          # Addition
+print("Sum=",(a+b))                 # Addition
 print("Difference=",(a-b))          # Subtraction
+print("Product=",(a*b))             # Multiplication
+print("Quotient=",(a/b))            # Quotient
