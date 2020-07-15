14ee2c86e5364d02fd81b775d74043f0feae6336
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-16 02:12:19 +0530

    Bitwise Operator

---
+++ Bitwise_operator.py
@@ -0,0 +1,8 @@
+a,b = 60, 2                #a = 0011 1100 , b = 0000 0010
+
+print(a&b)                 #Binary AND
+print(a|b)                 #Binary OR
+print(a^b)                 #Binary XOR
+print(~b)                  #Binary 1's complement
+print(a>>b)                #Binary Left shift
+print(a<<b)                #Binary Right Shift
