d0302d34d53c6da0ce1c0afa770150c9e90b30b5
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-16 03:13:05 +0530

    :

---
+++ Random_numbers.py
@@ -0,0 +1,6 @@
+import random
+s = 'abcde'
+print("choice(abcde):", random.choice(s))
+
+list = [10, 2, 3, 1, 8, 19]
+print("shuffle(list):", random.shuffle(list))                            # shuffles the item in list, returns none
