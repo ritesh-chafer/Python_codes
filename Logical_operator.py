8f7cd6cd1807469c005aeef92c2edcf116905445
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-16 02:28:11 +0530

    Logical Operator

---
+++ Logical_operator.py
@@ -0,0 +1,5 @@
+a, b, c, d = 10,5, 2, 1
+
+print((a>b) and (c>d))                   # Logical AND
+print((a>b) or (d>c))                    # Logical OR                    
+print(not(a>b))                          # Logical NOT
