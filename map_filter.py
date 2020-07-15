dc10b99797f3b3017ec056e0aca23d74737ac26d
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-15 21:09:07 +0530

    map function

--- map_filter.py
+++ map_filter.py
@@ -1,14 +1,27 @@
-numbers = ["3","4","1"]
+# numbers = ["3","4","1"]
 
-numbers = list(map(int, numbers))
+# numbers = list(map(int, numbers))
 
 # for i in range(len(numbers)):
 #     numbers[i] = int(numbers[i])
 
-numbers[2] = numbers[2] + 1
+# numbers[2] = numbers[2] + 1
 
 # print(numbers[2])
 
-num = [1,2,3,4,5,6,7,8,9]
-square = list(map(lambda x:x*x,num))
-print(square)
+# num = [1,2,3,4,5,6,7,8,9]
+# square = list(map(lambda x:x*x,num))
+# print(square)
+
+# def square(a):
+#     return a*a
+
+# def cube(a):
+#     return a*a*a
+
+# func = [square,cube]
+# numbers = ["3","4","1"]
+
+# for i in range(5):
+#     val = list(map(lambda x:x(i),func))
+#     print(val)
