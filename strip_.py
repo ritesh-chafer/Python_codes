8f6745e70c67cfd5b29bb6923c162baeca2116be
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-17 22:37:43 +0530

    :

--- strip_.py
+++ strip_.py
@@ -4,6 +4,6 @@ s = 'learning Python is fun!******'
 print(s.rstrip('*'))
 
 s = '       learning Python is fun!'
-print(s.rstrip())
+print(s.lstrip())
 s = '******learning Python is fun!'
-print(s.rstrip('*'))
+print(s.lstrip('*'))
