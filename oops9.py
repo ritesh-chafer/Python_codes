ae725a6ec18dafd8ce5333ce373027ed1de6437c
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-22 20:03:57 +0530

    :

--- oops9.py
+++ oops9.py
@@ -1,1 +1,21 @@
 # Multilevel Inheritance
+
+class Dad:
+    basketball = 1
+
+class Son(Dad):
+    dance = 1
+    def isdance(self):
+        return f"Yes I dance {self.dance} number of times."
+
+class Grandson(Son):
+    dance = 6
+    def isdance(self):
+        return f"I love Jackson!" \
+            f"Yes I dance too {self.dance} number of times."
+            
+harry = Dad()
+marrie = Son()
+jenny = Grandson() 
+
+print(jenny.isdance())
