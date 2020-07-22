6e397bf4503e3e5537f2bce03fec1f400b01fb3b
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-22 12:43:43 +0530

    :

---
+++ oops5.py
@@ -0,0 +1,35 @@
+class Employee:
+    no_of_leaves = 8
+
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+    
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
+        
+    @classmethod
+    def from_str(cls, string):                        
+        params = string.split("-")
+        return cls(params[0],params[1],params[2])
+        
+   
+
+harry = Employee("Harry", 255, "Instructor")
+rohan = Employee.from_str("Rohan-700-Student")
+
+# rohan = Employee()
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+
+print(rohan.salary)
