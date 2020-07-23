19ccf7fa6256c4d8f482cb187c18e30ba50f1577
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-23 18:52:21 +0530

    :

---
+++ oops10.py
@@ -0,0 +1,28 @@
+# concept for Public, Protected and Private access specifiers
+
+class Employee:
+    no_of_leaves = 8
+
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+    
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
+        
+    @classmethod
+    def from_dash(cls, string):                        #constructor
+        # params = string.split("-")
+        # return cls(params[0],params[1],params[2])
+        return cls(*string.split("-"))
+    
+    @staticmethod
+    def printgood(string):
+        print("This is good", string)
+
+
