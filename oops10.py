041301aca5b200aaa7ef496662941fb575b62ebb
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-23 18:55:56 +0530

    :

--- oops10.py
+++ oops10.py
@@ -3,7 +3,7 @@
 class Employee:
     no_of_leaves = 8
     var = 8                            # Public variable
-    _protected_var = 9
+    _protected_var = 9                 # Protected variable
     __private_var = 10
 
     def __init__(self, aname, asalary, arole):
