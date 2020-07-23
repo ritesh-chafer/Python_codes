41e4112906c0fc2e4662f87eb328cfb264056325
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-23 21:43:38 +0530

    :

--- oops12.py
+++ oops12.py
@@ -9,9 +9,10 @@ class A:
 class B(A):
     classvar1 = "I am in class B"
     def __init__(self):
-        super().__init__()
         self.var1 = "I am insde clas B's constructor"
         self.classvar1 = "Instance variable in class B"
+        super().__init__()
+
     
     
 a = A()
