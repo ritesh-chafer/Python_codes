a1526fda1c397b08031c25d4f02543392dbef913
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-23 20:55:03 +0530

    :

--- oops12.py
+++ oops12.py
@@ -1,1 +1,12 @@
 # super() and overriding in class
+class A:
+    classvar1 = "I am a calss variable in class A"
+    def __init__(self):
+        self.var1 = "I am insde clas A's constructor"
+        
+class B(A):
+    classvar2 = "I am in class B"
+    
+    
+a = A()
+b = B()
