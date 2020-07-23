caf2858c9177e84636819ec0da52ffa73bed2c08
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-23 20:57:31 +0530

    :

--- oops12.py
+++ oops12.py
@@ -3,6 +3,7 @@ class A:
     classvar1 = "I am a calss variable in class A"
     def __init__(self):
         self.var1 = "I am insde clas A's constructor"
+        self.classvar1 = "Instance variable in class A"
         
 class B(A):
     classvar2 = "I am in class B"
