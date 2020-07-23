b52a3f711d2903892b28e762d0f10242c3e6aac7
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-23 20:12:40 +0530

    :

--- oops11.py
+++ oops11.py
@@ -1,27 +1,6 @@
 # Polymorphism
+# Ability to take various forms
 
-class Employee:
-    no_of_leaves = 8
-    var = 8
 
-    def __init__(self, aname, asalary, arole):
-        self.name = aname
-        self.salary = asalary
-        self.role = arole
-
-    def printdetails(self):
-        return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
-    
-    @classmethod
-    def change_leaves(cls, newleaves):
-        cls.no_of_leaves = newleaves
-        
-    @classmethod
-    def from_dash(cls, string):                        #constructor
-        # params = string.split("-")
-        # return cls(params[0],params[1],params[2])
-        return cls(*string.split("-"))
-    
-    @staticmethod
-    def printgood(string):
-        print("This is good", string)
+print(5 + 6)
+print("5" + "6")
