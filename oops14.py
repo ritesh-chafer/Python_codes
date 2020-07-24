aac6cc5d6b51e569488cab180b53c12d2c0d29b7
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-24 18:39:48 +0530

    :

--- oops14.py
+++ oops14.py
@@ -1,1 +1,17 @@
 # Operator Overloading and Dunder method
+# Method starting with "__" and ending with "__" is known as dunder method e.g., "__init__ "(known as dunter init)
+
+class Employee:
+    no_of_leaves = 8
+
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+    
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
