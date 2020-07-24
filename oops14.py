6be2be3716f2a96dc079665a3825445d0ee4b96e
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-24 18:46:23 +0530

    :

--- oops14.py
+++ oops14.py
@@ -15,6 +15,11 @@ class Employee:
     @classmethod
     def change_leaves(cls, newleaves):
         cls.no_of_leaves = newleaves
+    
+    def __add__(self, other):
+        return self.salary + other.salary
         
 emp1 = Employee("Harry", 777, "Programmer")
 emp2 = Employee("Ritesh", 545, "Learner")
+
+print(emp1 + emp2)
