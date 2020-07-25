f042b276282873b64515dc64d185c3bde4a450fc
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-25 20:34:58 +0530

    :

--- Setters_Property_Deorators.py
+++ Setters_Property_Deorators.py
@@ -2,3 +2,10 @@ class Employee:
     def __init(self, f_name, l_name):
         self.fname = f_name
         self.lnmae = l_name
+        
+    def explain(self):
+        return f"This employee is {self.fname} {self.lname}"
+    
+    
+E1 = Employee()
+E2 = Employee()
