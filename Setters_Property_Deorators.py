3f6d6456bc7b52128ce9b811b3b50cab7296b3e6
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-26 20:30:39 +0530

    :

--- Setters_Property_Deorators.py
+++ Setters_Property_Deorators.py
@@ -9,20 +9,35 @@ class Employee:
     def explain(self):
         return f"This employee is {self.fname} {self.lname}"
     
+    @property
     def email(self):
+        if self.fname== None or self.lname == None:
+            return "Email is not set. Please set it using setter."
         return f"{self.fname}.{self.lname}@chafer.com"
     
     @email.setter
     def email(self, string):
         names = string.split("@")[0]
         self.fname = names.split(".")[0]
-        self.lname = names.split(".")[1]
+        self.lname = names.split(".")[1]\
+    
+    @email.deleter
+    def email(self):
+        self.fname = None
+        self.lname = None
     
 E1 = Employee("Rahul", "Singh")
 E2 = Employee("Raj", "Kapoor")
 
-print(E1.email())
+print(E1.email)
 
 E1.fname = "Mayank"
 
-print(E1.email())
+print(E1.email)
+
+E1.email = "this.that@chafer.com"
+
+del E1.email
+
+
+print(E1.email)
