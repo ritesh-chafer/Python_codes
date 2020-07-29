0a0b7e7cc1b098ae71796df9fb7a0f98171ac571
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-29 20:37:05 +0530

    :

--- comprehensions_.py
+++ comprehensions_.py
@@ -5,6 +5,6 @@
 #     if i % 3 == 0:
 #         ls.append(i)
         
-ls = [i for i in range(100) if( i%3 == 0)]        
+# ls = [i for i in range(100) if( i%3 == 0)]        
 
-print(ls)
+# print(ls)
