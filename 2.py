60dc8b97ba759358f2b6400bf9b1c85edb12a4e9
Ritesh Prasad Singh <riteshprasadsingh8@gmail.com>
2020-08-01 20:45:34 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,10 +4,10 @@
 
 
 
-#Hours = int(input("Enter Hours: "))
-#Rate = float(input("Enter Rate: "))
-#Pay = (float(round(Hours,2)) * float(round(Rate,2)))
-#print("Pay: ",Pay )
+Hours = int(input("Enter Hours: "))
+Rate = float(input("Enter Rate: "))
+Pay = (float(round(Hours,2)) * float(round(Rate,2)))
+print("Pay: ",Pay )
 
 
 
