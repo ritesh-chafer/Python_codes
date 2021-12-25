9a19fe2b061142a2e441f61c2951e81b29f57470
ritesh_chafer <riteshprasadsingh8@gmail.com>
2021-12-25 19:36:12 +0530

    sorted

---
+++ Sorting/sort_sorted.py
@@ -0,0 +1,8 @@
+number_tuple = (1,4,0,2,5,6)
+number_set = {1,4,0,2,5,6}
+
+number_tuple_sorted = sorted(number_tuple)
+number_set_sorted = sorted(number_set)
+
+print(number_tuple_sorted)
+print(number_set_sorted)
